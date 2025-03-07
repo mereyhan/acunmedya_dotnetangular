bacf1b553ef048863510c1b5f1edcd660763d35c
merey <mereyhan@outlook.com>
2025-03-07 13:15:30 +0300

    Banka hesabı ve kullanıcı sınıflarının eklenmesi

---
+++ Day04_InheritanceAndPolymorphism/BankOperations/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04_InheritanceAndPolymorphism.BankOperations
+{
+    class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
+
+        public virtual void CalculateInterest() { }
+    }
+}
---
+++ Day04_InheritanceAndPolymorphism/Staff/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04_InheritanceAndPolymorphism.Staff
+{
+    class Manager : Employee
+    {
+        public int TeamSize { get; set; }
+    }
+}
